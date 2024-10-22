cd4797a0dc55379a1959387e80240631d0db6bf9
Stream <1542763342@qq.com>
2024-10-22 21:49:37 +0800

    refactor(core): use data object on MessageSender

--- core/src/commonMain/kotlin/chat/message/MessageSender.kt
+++ core/src/commonMain/kotlin/chat/message/MessageSender.kt
@@ -1,9 +1,7 @@
 package chat.message
 
-sealed class MessageSender(private val name: String) {
-    object User : MessageSender("user")
-    object System : MessageSender("system")
-    object Model : MessageSender("model")
-
-    override fun toString() = name
+sealed interface MessageSender {
+    data object User : MessageSender
+    data object System : MessageSender
+    data object Model : MessageSender
 }
