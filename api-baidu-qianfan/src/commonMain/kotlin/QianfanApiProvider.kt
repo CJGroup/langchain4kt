c46dc7eeada354930e53a78c19e5a4bcb0ac3650
Stream <1542763342@qq.com>
2024-10-23 15:12:35 +0800

    feat(api-baidu-qianfan): basic todo

--- api-baidu-qianfan/src/commonMain/kotlin/QianfanApiProvider.kt
+++ api-baidu-qianfan/src/commonMain/kotlin/QianfanApiProvider.kt
@@ -1,7 +1,14 @@
 import chat.ChatApiProvider
+import chat.input.Context
+import chat.output.Response
+import io.ktor.client.*
 
 class QianfanApiProvider(
-
-): ChatApiProvider<*,*> {
-
+    val httpClient: HttpClient,
+    val apiKey: String,
+    val secretKey: String,
+): ChatApiProvider<Unit,Unit> {
+    override suspend fun generate(context: Context): Response<*, Unit, Unit> {
+        TODO("Not yet implemented")
+    }
 }
