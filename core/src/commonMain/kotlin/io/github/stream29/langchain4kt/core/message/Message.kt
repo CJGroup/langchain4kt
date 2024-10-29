6fc81815f3a861eca07e3ac7af4d97e1dbada3c9
Stream <1542763342@qq.com>
2024-10-29 09:11:12 +0800

    refactor: add type parameter to MessageType
    For future extension

--- core/src/commonMain/kotlin/io/github/stream29/langchain4kt/core/message/Message.kt
+++ core/src/commonMain/kotlin/io/github/stream29/langchain4kt/core/message/Message.kt
@@ -1,7 +1,7 @@
 package io.github.stream29.langchain4kt.core.message
 
 data class Message<Content>(
-    val type: MessageType,
+    val type: MessageType<Content>,
     val sender: MessageSender,
     val content: Content
 ) {
