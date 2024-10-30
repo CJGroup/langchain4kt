583a70db10a6b00352b7cc96925e65c3d6e082d1
Stream <1542763342@qq.com>
2024-10-30 14:22:47 +0800

    feat(core): add shortcut for ChatApiProvider

--- core/src/commonMain/kotlin/io/github/stream29/langchain4kt/core/ChatApiProvider.kt
+++ core/src/commonMain/kotlin/io/github/stream29/langchain4kt/core/ChatApiProvider.kt
@@ -1,9 +1,17 @@
 package io.github.stream29.langchain4kt.core
 
+import io.github.stream29.langchain4kt.core.dsl.ContextBuilder
+import io.github.stream29.langchain4kt.core.dsl.of
 import io.github.stream29.langchain4kt.core.input.Context
-import io.github.stream29.langchain4kt.core.message.Message
+import io.github.stream29.langchain4kt.core.message.MessageSender
 import io.github.stream29.langchain4kt.core.output.Response
 
 interface ChatApiProvider<MetaInfo> {
     suspend fun generate(context: Context): Response<MetaInfo>
 }
+
+suspend fun <T> ChatApiProvider<T>.generateFrom(block: ContextBuilder.() -> Unit) =
+    generate(Context.of(block))
+
+suspend fun ChatApiProvider<*>.generate(userMessage: String) =
+    generateFrom { MessageSender.User.chat(userMessage) }.message
