478cb9032ae8c1a3053f203bad5eee5242f8f162
Stream <1542763342@qq.com>
2024-10-30 13:52:46 +0800

    feat: add memory example

---
+++ example-model-memory/src/commonMain/kotlin/io/github/stream29/langchain4kt/api/baiduqianfan/MetapromptChatModel.kt
@@ -0,0 +1,34 @@
+package io.github.stream29.langchain4kt.api.baiduqianfan
+
+import io.github.stream29.langchain4kt.core.ChatApiProvider
+import io.github.stream29.langchain4kt.core.ChatModel
+import io.github.stream29.langchain4kt.core.dsl.add
+import io.github.stream29.langchain4kt.core.dsl.of
+import io.github.stream29.langchain4kt.core.input.Context
+import io.github.stream29.langchain4kt.core.message.MessageSender
+
+class MetapromptChatModel(
+    override val context: Context = Context(),
+    var apiProvider: ChatApiProvider<*>,
+    var metapromptTransform: (String) -> String
+) : ChatModel {
+    override suspend fun chat(message: String): String {
+        val metaprompt = metapromptTransform(message)
+        val prompt =
+            apiProvider.generate(
+                Context.of {
+                    MessageSender.User.chat(metaprompt)
+                }
+            ).message
+
+        val response = apiProvider.generate(
+            context.copy().apply { add { MessageSender.User.chat(prompt) } }
+        ).message
+
+        context.add {
+            MessageSender.User.chat(message)
+            MessageSender.Model.chat(response)
+        }
+        return response
+    }
+}
