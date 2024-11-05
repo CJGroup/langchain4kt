d0d7f0cbdfd530134ac161a89fb0a5054e43ef22
Stream <1542763342@qq.com>
2024-11-05 16:59:31 +0800

    feat(api-baidu-qianfan): fetch accessToken on accessToken expired

--- api-baidu-qianfan/src/commonMain/kotlin/io/github/stream29/langchain4kt/api/baiduqianfan/QianfanApiProvider.kt
+++ api-baidu-qianfan/src/commonMain/kotlin/io/github/stream29/langchain4kt/api/baiduqianfan/QianfanApiProvider.kt
@@ -7,6 +7,7 @@ import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.serialization.SerializationException
@@ -57,6 +58,10 @@ class QianfanApiProvider(
             )
         } catch (e: SerializationException) {
             val error = json.decodeFromString<RequestError>(responseBody)
+            if(error.errorCode == 110) {
+                accessToken = null
+                return generate(context)
+            }
             throw QianFanGenerationException(error)
         }
     }
