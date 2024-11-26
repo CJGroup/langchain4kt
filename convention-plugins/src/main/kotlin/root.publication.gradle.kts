ebb2902e7131d4ffa03036f87712585a14f2d0bf
Stream <1542763342@qq.com>
2024-11-26 11:28:38 +0800

    chore: set version to 1.3.1

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -1,20 +1,4 @@
-plugins {
-//    id("io.github.gradle-nexus.publish-plugin")
-
-}
-
 allprojects {
     group = "io.github.stream29"
-    version = "1.3.0"
+    version = "1.3.1"
 }
-
-//nexusPublishing {
-//    // Configure maven central repository
-//    // https://github.com/gradle-nexus/publish-plugin#publishing-to-maven-central-via-sonatype-ossrh
-//    repositories {
-//        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
-//            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-//            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-//        }
-//    }
-//}
