e7194cfc3b4d9eeda52933e298143adefd1dac21
Stream <1542763342@qq.com>
2025-01-15 12:04:43 +0800

    refactor(api-openai): use buildList

--- api-openai/src/commonMain/kotlin/io/github/stream29/langchain4kt/api/openai/MessageSenderMapping.kt
+++ api-openai/src/commonMain/kotlin/io/github/stream29/langchain4kt/api/openai/MessageSenderMapping.kt
@@ -6,24 +6,27 @@ import io.github.stream29.langchain4kt.core.input.Context
 import io.github.stream29.langchain4kt.core.message.MessageSender
 
 internal fun MessageSender.toOpenAiRole(): Role =
-    when(this) {
+    when (this) {
         MessageSender.User -> Role.User
         MessageSender.Model -> Role.Assistant
     }
 
-internal fun Context.toOpenAiMessageList(): List<ChatMessage> {
-    val messageList = mutableListOf<ChatMessage>()
-    systemInstruction?.let {
-        ChatMessage(
-            role = Role.System,
-            content = it
-        )
-    }?.let { messageList.add(it) }
-    history.asSequence().map {
-        ChatMessage(
-            role = it.sender.toOpenAiRole(),
-            content = it.content
-        )
-    }.forEach { messageList.add(it) }
-    return messageList
-}
+internal fun Context.toOpenAiMessageList(): List<ChatMessage> =
+    buildList {
+        systemInstruction?.let {
+            add(
+                ChatMessage(
+                    role = Role.System,
+                    content = it
+                )
+            )
+        }
+        history.forEach {
+            add(
+                ChatMessage(
+                    role = it.sender.toOpenAiRole(),
+                    content = it.content
+                )
+            )
+        }
+    }
