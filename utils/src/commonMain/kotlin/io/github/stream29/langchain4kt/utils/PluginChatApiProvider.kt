722e7c6439f674d70b6a58126ab8be59be2acb9e
Stream <1542763342@qq.com>
2025-01-16 14:17:28 +0800

    refactor(utils): rewrite plugin system

--- utils/src/commonMain/kotlin/io/github/stream29/langchain4kt/utils/PluginChatApiProvider.kt
+++ utils/src/commonMain/kotlin/io/github/stream29/langchain4kt/utils/PluginChatApiProvider.kt
@@ -12,90 +12,90 @@ import io.github.stream29.langchain4kt.streaming.StreamRespondent
 import io.github.stream29.langchain4kt.streaming.StreamResponse
 import kotlinx.coroutines.flow.Flow
 
-public typealias ChatApiProviderPlugin<T> = GeneratorPlugin<Context, Response<T>>
-public typealias RespondentPlugin = GeneratorPlugin<String, String>
-public typealias ChatModelPlugin = GeneratorPlugin<String, String>
-public typealias EmbeddingApiProviderPlugin<T> = GeneratorPlugin<String, T>
-public typealias StreamChatApiProviderPlugin<T> = GeneratorPlugin<Context, StreamResponse<T>>
-public typealias StreamRespondentPlugin = GeneratorPlugin<String, Flow<String>>
-public typealias StreamChatModelPlugin = GeneratorPlugin<String, Flow<String>>
+public typealias ChatApiProviderFunc<MetaInfo> = Generator<Context, Response<MetaInfo>>
+public typealias RespondentFunc = Generator<String, String>
+public typealias ChatModelFunc = Generator<String, String>
+public typealias EmbeddingApiProviderFunc<T> = Generator<String, T>
+public typealias StreamChatApiProviderFunc<MetaInfo> = Generator<Context, StreamResponse<MetaInfo>>
+public typealias StreamRespondentFunc = Generator<String, Flow<String>>
+public typealias StreamChatModelFunc = Generator<String, Flow<String>>
 
 /**
  * A [ChatApiProvider] that uses a plugin to modify the behavior of the provider.
  * @property baseChatApiProvider [ChatApiProvider] to use
- * @property aroundGenerate Plugin to use
+ * @property plugin Plugin to use
  */
 public class PluginChatApiProvider<MetaInfo>(
     public val baseChatApiProvider: ChatApiProvider<MetaInfo>,
-    public val aroundGenerate: ChatApiProviderPlugin<MetaInfo>,
+    public val plugin: Plugin<ChatApiProviderFunc<MetaInfo>>,
 ) : ChatApiProvider<MetaInfo> {
     override suspend fun generate(context: Context): Response<MetaInfo> {
-        return aroundGenerate(context, baseChatApiProvider::generate)
+        return plugin(baseChatApiProvider::generate)(context)
     }
 }
 
 public class PluginRespondent(
     public val baseRespondent: Respondent,
-    public val aroundChat: RespondentPlugin,
+    public val aroundChat: Plugin<RespondentFunc>,
 ) : Respondent {
     override suspend fun chat(message: String): String {
-        return aroundChat(message, baseRespondent::chat)
+        return aroundChat(baseRespondent::chat)(message)
     }
 }
 
 public class PluginChatModel(
     public val baseChatModel: ChatModel,
-    public val aroundChat: ChatModelPlugin
+    public val aroundChat: Plugin<ChatModelFunc>
 ) : ChatModel {
     public override val context: Context by baseChatModel::context
     override suspend fun chat(message: String): String {
-        return aroundChat(message, baseChatModel::chat)
+        return aroundChat(baseChatModel::chat)(message)
     }
 }
 
 public class PluginEmbeddingApiProvider<T>(
     public val baseEmbeddingApiProvider: EmbeddingApiProvider<T>,
-    public val aroundEmbed: EmbeddingApiProviderPlugin<T>,
+    public val aroundEmbed: Plugin<EmbeddingApiProviderFunc<T>>,
 ) : EmbeddingApiProvider<T> {
     override suspend fun embed(text: String): T {
-        return aroundEmbed(text, baseEmbeddingApiProvider::embed)
+        return aroundEmbed(baseEmbeddingApiProvider::embed)(text)
     }
 }
 
 public class PluginStreamChatApiProvider<MetaInfo>(
     public val baseStreamChatApiProvider: StreamChatApiProvider<MetaInfo>,
-    public val aroundGenerate: StreamChatApiProviderPlugin<MetaInfo>,
+    public val aroundGenerate: Plugin<StreamChatApiProviderFunc<MetaInfo>>,
 ) : StreamChatApiProvider<MetaInfo> {
     override suspend fun generate(context: Context): StreamResponse<MetaInfo> {
-        return aroundGenerate(context, baseStreamChatApiProvider::generate)
+        return aroundGenerate(baseStreamChatApiProvider::generate)(context)
     }
 }
 
 public class PluginStreamRespondent(
     public val baseStreamRespondent: StreamRespondent,
-    public val aroundChat: StreamRespondentPlugin
+    public val aroundChat: Plugin<StreamRespondentFunc>
 ) : StreamRespondent {
     override suspend fun chat(message: String): Flow<String> {
-        return aroundChat(message, baseStreamRespondent::chat)
+        return aroundChat(baseStreamRespondent::chat)(message)
     }
 }
 
 public class PluginStreamChatModel(
     public val baseStreamChatModel: StreamChatModel,
-    public val aroundChat: StreamChatModelPlugin
+    public val aroundChat: Plugin<StreamChatModelFunc>
 ) : StreamChatModel {
     public override val context: Context by baseStreamChatModel::context
     public override val isReady: Boolean by baseStreamChatModel::isReady
     override suspend fun chat(message: String): Flow<String> {
-        return aroundChat(message, baseStreamChatModel::chat)
+        return aroundChat(baseStreamChatModel::chat)(message)
     }
 }
 
 /**
- * Install a list of [ChatApiProviderPlugin] to a [ChatApiProvider].
+ * Install a list of [Plugin] to a [ChatApiProvider].
  * @param plugins List of plugins to install
  */
-public fun <T> ChatApiProvider<T>.install(vararg plugins: ChatApiProviderPlugin<T>): ChatApiProvider<T> =
+public fun <MetaInfo> ChatApiProvider<MetaInfo>.install(vararg plugins: Plugin<ChatApiProviderFunc<MetaInfo>>): ChatApiProvider<MetaInfo> =
     plugins.fold(this) { chatApiProvider, plugin ->
         PluginChatApiProvider(
             chatApiProvider,
@@ -104,10 +104,10 @@ public fun <T> ChatApiProvider<T>.install(vararg plugins: ChatApiProviderPlugin<
     }
 
 /**
- * Install a list of [RespondentPlugin] to a [Respondent].
+ * Install a list of [Plugin] to a [Respondent].
  * @param plugins List of plugins to install
  */
-public fun Respondent.install(vararg plugins: RespondentPlugin): Respondent =
+public fun Respondent.install(vararg plugins: Plugin<RespondentFunc>): Respondent =
     plugins.fold(this) { respondent, plugin ->
         PluginRespondent(
             respondent,
@@ -115,7 +115,7 @@ public fun Respondent.install(vararg plugins: RespondentPlugin): Respondent =
         )
     }
 
-public fun ChatModel.install(vararg plugins: ChatModelPlugin): ChatModel =
+public fun ChatModel.install(vararg plugins: Plugin<ChatModelFunc>): ChatModel =
     plugins.fold(this) { chatModel, plugin ->
         PluginChatModel(
             chatModel,
@@ -123,7 +123,7 @@ public fun ChatModel.install(vararg plugins: ChatModelPlugin): ChatModel =
         )
     }
 
-public fun <T> EmbeddingApiProvider<T>.install(vararg plugins: EmbeddingApiProviderPlugin<T>): EmbeddingApiProvider<T> =
+public fun <T> EmbeddingApiProvider<T>.install(vararg plugins: Plugin<EmbeddingApiProviderFunc<T>>): EmbeddingApiProvider<T> =
     plugins.fold(this) { embeddingApiProvider, plugin ->
         PluginEmbeddingApiProvider(
             embeddingApiProvider,
@@ -132,11 +132,11 @@ public fun <T> EmbeddingApiProvider<T>.install(vararg plugins: EmbeddingApiProvi
     }
 
 /**
- * Install a list of [ChatApiProviderPlugin] to a [StreamChatApiProvider].
+ * Install a list of [Plugin] to a [StreamChatApiProvider].
  * The behaviour of plugin is just like that on [ChatApiProvider], but waiting for the stream generation complete.
  * @param plugins List of plugins to install
  */
-public fun <T> StreamChatApiProvider<T>.install(vararg plugins: StreamChatApiProviderPlugin<T>): StreamChatApiProvider<T> =
+public fun <T> StreamChatApiProvider<T>.install(vararg plugins: Plugin<StreamChatApiProviderFunc<T>>): StreamChatApiProvider<T> =
     plugins.fold(this) { chatApiProvider, plugin ->
         PluginStreamChatApiProvider(
             chatApiProvider,
@@ -144,7 +144,7 @@ public fun <T> StreamChatApiProvider<T>.install(vararg plugins: StreamChatApiPro
         )
     }
 
-public fun StreamRespondent.install(vararg plugins: StreamRespondentPlugin): StreamRespondent =
+public fun StreamRespondent.install(vararg plugins: Plugin<StreamRespondentFunc>): StreamRespondent =
     plugins.fold(this) { respondent, plugin ->
         PluginStreamRespondent(
             respondent,
@@ -152,7 +152,7 @@ public fun StreamRespondent.install(vararg plugins: StreamRespondentPlugin): Str
         )
     }
 
-public fun StreamChatModel.install(vararg plugins: StreamChatModelPlugin): StreamChatModel =
+public fun StreamChatModel.install(vararg plugins: Plugin<StreamChatModelFunc>): StreamChatModel =
     plugins.fold(this) { chatModel, plugin ->
         PluginStreamChatModel(
             chatModel,
--- utils/src/commonMain/kotlin/io/github/stream29/langchain4kt/utils/Plugins.kt
+++ utils/src/commonMain/kotlin/io/github/stream29/langchain4kt/utils/Plugins.kt
@@ -1,23 +1,21 @@
 package io.github.stream29.langchain4kt.utils
 
 import io.github.stream29.langchain4kt.core.output.GenerationException
-import kotlinx.coroutines.delay
 import kotlin.time.Duration
 import kotlin.time.measureTimedValue
 
+public typealias Plugin<C> = (C) -> C
 public typealias Generator<T, R> = suspend (T) -> R
-public typealias GeneratorPlugin<T, R> = suspend (T, Generator<T, R>) -> R
+public typealias PluginImplementation<C, T, R> = suspend (C, T) -> R
+public typealias PluginReceiver<C, T, R> = (C, Plugin<Generator<T, R>>) -> C
+
+public inline fun <T, R> curry(crossinline block: suspend (Generator<T, R>, T) -> R): Plugin<Generator<T, R>> =
+    { p0 -> { p1 -> block(p0, p1) } }
 
 /**
- * Collection of basic plugins for [io.github.stream29.langchain4kt.core.ChatApiProvider].
+ * Collection of basic plugins.
  */
 public object Plugins {
-    /**
-     * Plugin that logs the generation process.
-     * @param output Destination to output the log
-     * @param successFormat Format for successful generation
-     * @param failureFormat Format for failed generation
-     */
     public inline fun <T, R> logging(
         crossinline output: (String) -> Unit = ::println,
         crossinline successFormat: (T, R, Duration) -> String = { param, result, duration ->
@@ -26,48 +24,76 @@ public object Plugins {
         crossinline failureFormat: (T, Throwable, Duration) -> String = { param, throwable, duration ->
             "Generation failed in $duration with param \n$param\n and exception \n$throwable"
         },
-    ): GeneratorPlugin<T, R> = { param, generate ->
-        val timedValue = measureTimedValue {
+    ): Plugin<Generator<T, R>> = curry { respondent, param ->
+        val (result, duration) = measureTimedValue {
             runCatching {
-                generate(param)
+                respondent(param)
             }
         }
-        val duration = timedValue.duration
-        timedValue.value.onSuccess { response ->
+        result.onSuccess { response ->
             output(successFormat(param, response, duration))
         }.onFailure { throwable ->
             output(failureFormat(param, throwable, duration))
         }.getOrThrow()
     }
 
-    /**
-     * Plugin that retries the generation process on failure.
-     * @param times Number of times to retry
-     * @param delayMillis Delay between retries
-     */
-    public fun <T, R> retryOnFail(
-        times: Int = 5,
-        delayMillis: Long = 0,
-        retryExceeded: (param: T, times: Int, throwable: Throwable) -> Nothing = ::throwRetryException
-    ): GeneratorPlugin<T, R> = plugin@{ param, generate ->
-        var count = 0
-        while (true) {
-            runCatching {
-                return@plugin generate(param)
-            }.onFailure {
-                if (count >= times) retryExceeded(param, count, it)
-                count++
-                delay(delayMillis)
-            }
-        }
-        TODO("Unreachable code")
-    }
 
-    public inline fun metaprompt(
-        crossinline metaprompt: suspend (String) -> String
-    ): RespondentPlugin = { message, generate ->
-        generate(generate(metaprompt(message)))
-    }
+//    /**
+//     * Plugin that logs the generation process.
+//     * @param output Destination to output the log
+//     * @param successFormat Format for successful generation
+//     * @param failureFormat Format for failed generation
+//     */
+//    public inline fun <T, R> logging(
+//        crossinline output: (String) -> Unit = ::println,
+//        crossinline successFormat: (T, R, Duration) -> String = { param, result, duration ->
+//            "Generation completed in $duration with param \n$param\n and result \n$result"
+//        },
+//        crossinline failureFormat: (T, Throwable, Duration) -> String = { param, throwable, duration ->
+//            "Generation failed in $duration with param \n$param\n and exception \n$throwable"
+//        },
+//    ): GeneratorPlugin<T, R> = { param, generate ->
+//        val timedValue = measureTimedValue {
+//            runCatching {
+//                generate(param)
+//            }
+//        }
+//        val duration = timedValue.duration
+//        timedValue.value.onSuccess { response ->
+//            output(successFormat(param, response, duration))
+//        }.onFailure { throwable ->
+//            output(failureFormat(param, throwable, duration))
+//        }.getOrThrow()
+//    }
+//
+//    /**
+//     * Plugin that retries the generation process on failure.
+//     * @param times Number of times to retry
+//     * @param delayMillis Delay between retries
+//     */
+//    public fun <T, R> retryOnFail(
+//        times: Int = 5,
+//        delayMillis: Long = 0,
+//        retryExceeded: (param: T, times: Int, throwable: Throwable) -> Nothing = ::throwRetryException
+//    ): GeneratorPlugin<T, R> = plugin@{ param, generate ->
+//        var count = 0
+//        while (true) {
+//            runCatching {
+//                return@plugin generate(param)
+//            }.onFailure {
+//                if (count >= times) retryExceeded(param, count, it)
+//                count++
+//                delay(delayMillis)
+//            }
+//        }
+//        TODO("Unreachable code")
+//    }
+
+//    public inline fun metaprompt(
+//        crossinline metaprompt: suspend (String) -> String
+//    ): RespondentPlugin = { message, generate ->
+//        generate(generate(metaprompt(message)))
+//    }
 
     public fun <T> throwRetryException(param: T, times: Int, cause: Throwable): Nothing =
         throw GenerationException("Generation failed after $times retries with context: \n$param", cause)
