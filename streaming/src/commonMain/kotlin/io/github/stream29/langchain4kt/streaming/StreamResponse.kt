7814fd3896c208cc2f50f17f974f8bf8dd3c968f
Stream <1542763342@qq.com>
2025-03-02 16:44:01 +0800

    refactor: remove modules

--- streaming/src/commonMain/kotlin/io/github/stream29/langchain4kt/streaming/StreamResponse.kt
+++
@@ -1,9 +0,0 @@
-package io.github.stream29.langchain4kt.streaming
-
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.flow.Flow
-
-public data class StreamResponse<MetaInfo>(
-    val message: Flow<String>,
-    val metaInfo: Deferred<MetaInfo>
-)
