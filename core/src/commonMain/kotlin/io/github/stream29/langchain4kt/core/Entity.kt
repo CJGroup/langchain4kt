cbe9024431d8b1ee109503a1aa9e2f9d46631bae
Stream <1542763342@qq.com>
2025-03-03 08:44:40 +0800

    feat: work on entities

--- core/src/commonMain/kotlin/io/github/stream29/langchain4kt/core/Entity.kt
+++ core/src/commonMain/kotlin/io/github/stream29/langchain4kt/core/Entity.kt
@@ -1,16 +1,34 @@
 package io.github.stream29.langchain4kt.core
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 public data class Response<out Content, out MetaInfo>(
     val content: Content,
     val metaInfo: MetaInfo
 )
 
+@Serializable
 public enum class DataDirection {
     Input,
     Output
 }
 
+@Serializable
 public data class ChatMessage<out Content>(
     val direction: DataDirection,
     val content: Content
+)
+
+@Serializable
+public data class FunctionCallRequest<out Param>(
+    val functionId: String,
+    val param: Param
+)
+
+@Serializable
+public data class FunctionCallResponse<out Param, out Result>(
+    val functionId: String,
+    val param: Param,
+    val result: Result
 )
