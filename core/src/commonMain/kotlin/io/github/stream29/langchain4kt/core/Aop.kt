da269619e2e6a6cabc8e2bcfeb65835074a2bb2b
Stream <1542763342@qq.com>
2025-03-16 02:57:30 +0800

    feat(core): provide suspend and normal version for enhancing feature on Generator

--- core/src/commonMain/kotlin/io/github/stream29/langchain4kt/core/Aop.kt
+++ core/src/commonMain/kotlin/io/github/stream29/langchain4kt/core/Aop.kt
@@ -8,24 +8,45 @@ import kotlinx.coroutines.flow.toList
 public inline fun <Input, Output, NewInput, NewOutput> Generator<Input, Output>.wrapped(crossinline block: (Generator<Input, Output>) -> Generator<NewInput, NewOutput>): Generator<NewInput, NewOutput> =
     block(this)
 
-public inline fun <Input, Output, NewOutput> Generator<Input, Output>.mapOutput(crossinline block: suspend (Output) -> NewOutput): Generator<Input, NewOutput> =
+public inline fun <Input, Output, NewOutput> Generator<Input, Output>.mapOutput(crossinline block: (Output) -> NewOutput): Generator<Input, NewOutput> =
+    { input -> block(this(input)) }
+
+public inline fun <Input, Output, NewOutput> Generator<Input, Output>.mapOutputSuspend(crossinline block: suspend (Output) -> NewOutput): Generator<Input, NewOutput> =
     { input -> block(this(input)) }
 
 public inline fun <Input, OutputChunk, NewOutputChunk> Generator<Input, Flow<OutputChunk>>.mapOutputFlow(
+    crossinline block: (OutputChunk) -> NewOutputChunk
+): Generator<Input, Flow<NewOutputChunk>> = { input -> this(input).map { block(it) } }
+
+public inline fun <Input, OutputChunk, NewOutputChunk> Generator<Input, Flow<OutputChunk>>.mapOutputFlowSuspend(
     crossinline block: suspend (OutputChunk) -> NewOutputChunk
 ): Generator<Input, Flow<NewOutputChunk>> = { input -> this(input).map { block(it) } }
 
-public inline fun <Input, NewInput, Output> Generator<Input, Output>.mapInput(crossinline block: suspend (NewInput) -> Input): Generator<NewInput, Output> =
+public inline fun <Input, NewInput, Output> Generator<Input, Output>.mapInput(crossinline block: (NewInput) -> Input): Generator<NewInput, Output> =
     { input -> this(block(input)) }
 
-public inline fun <InputItem, NewInputItem, Output> Generator<History<InputItem>, Output>.mapInputHistory(crossinline block: suspend (NewInputItem) -> InputItem): Generator<History<NewInputItem>, Output> =
+public inline fun <Input, NewInput, Output> Generator<Input, Output>.mapInputSuspend(crossinline block: suspend (NewInput) -> Input): Generator<NewInput, Output> =
+    { input -> this(block(input)) }
+
+public inline fun <InputItem, NewInputItem, Output> Generator<History<InputItem>, Output>.mapInputHistory(crossinline block: (NewInputItem) -> InputItem): Generator<History<NewInputItem>, Output> =
+    { input -> this(input.map(block)) }
+
+public inline fun <InputItem, NewInputItem, Output> Generator<History<InputItem>, Output>.mapInputHistorySuspend(
+    crossinline block: suspend (NewInputItem) -> InputItem
+): Generator<History<NewInputItem>, Output> =
     { input -> this(input.asFlow().map(block).toList()) }
 
-public inline fun <Input, Output> Generator<Input, Output>.onInput(crossinline block: suspend (Input) -> Unit): Generator<Input, Output> =
+public inline fun <Input, Output> Generator<Input, Output>.onInput(crossinline block: (Input) -> Unit): Generator<Input, Output> =
     mapInput { block(it); it }
 
-public inline fun <Input, Output> Generator<Input, Output>.onOutput(crossinline block: suspend (Output) -> Unit): Generator<Input, Output> =
+public inline fun <Input, Output> Generator<Input, Output>.onInputSuspend(crossinline block: suspend (Input) -> Unit): Generator<Input, Output> =
+    mapInputSuspend { block(it); it }
+
+public inline fun <Input, Output> Generator<Input, Output>.onOutput(crossinline block: (Output) -> Unit): Generator<Input, Output> =
     mapOutput { block(it); it }
+
+public inline fun <Input, Output> Generator<Input, Output>.onOutputSuspend(crossinline block: suspend (Output) -> Unit): Generator<Input, Output> =
+    mapOutputSuspend { block(it); it }
 
 public inline fun <Input, OutputChunk> Generator<Input, Flow<OutputChunk>>.onOutputFlow(crossinline block: suspend (OutputChunk) -> Unit): Generator<Input, Flow<OutputChunk>> =
     mapOutput { it.map { block(it); it } }
@@ -42,7 +63,7 @@ public fun <Input, OutputElement> Generator<Input, List<OutputElement>>.mapOutpu
     { input -> this(input).first() }
 
 public fun <InputElement, Output> Generator<List<InputElement>, Output>.appendInputOn(baseList: List<InputElement>) =
-    mapInput { it: List<InputElement> -> baseList + it }
+    mapInputSuspend { it: List<InputElement> -> baseList + it }
 
 public fun <InputElement, Output> Generator<List<InputElement>, Output>.appendInputOn(baseElement: InputElement) =
     appendInputOn(listOf(baseElement))
