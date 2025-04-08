c8000ca77553bf9b777e970cae2ece2c52c18a7c
Stream <1542763342@qq.com>
2025-04-08 13:25:37 +0800

    build(mcp): add json schema generator dependency

--- mcp/build.gradle.kts
+++ mcp/build.gradle.kts
@@ -9,6 +9,7 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 api(libs.kunion)
+                api(libs.json.schema.generator)
                 api(libs.kotlinx.coroutine.core)
                 api(libs.kotlinx.serialization.core)
             }
