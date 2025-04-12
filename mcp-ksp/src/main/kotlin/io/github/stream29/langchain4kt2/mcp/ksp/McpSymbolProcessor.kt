3a5a374a5d5aaaa118b05e2e61db51b1b70b6174
Stream <1542763342@qq.com>
2025-04-12 08:58:54 +0800

    fix(mcp): fix usage of Kotlin poet and add utils to make it more DSL-styled

--- mcp-ksp/src/main/kotlin/io/github/stream29/langchain4kt2/mcp/ksp/McpSymbolProcessor.kt
+++ mcp-ksp/src/main/kotlin/io/github/stream29/langchain4kt2/mcp/ksp/McpSymbolProcessor.kt
@@ -6,11 +6,7 @@ import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSClassDeclaration
-import com.squareup.kotlinpoet.FileSpec
-import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.MemberName
-import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
-import com.squareup.kotlinpoet.asClassName
 import com.squareup.kotlinpoet.ksp.toClassName
 import com.squareup.kotlinpoet.ksp.writeTo
 import io.github.stream29.langchain4kt2.mcp.McpServerComponent
@@ -29,23 +25,25 @@ public class McpSymbolProcessor(private val environment: SymbolProcessorEnvironm
                     "io.github.stream29.langchain4kt2.mcp",
                     "makeTool"
                 )
-                val function = FunSpec.builder("tools")
-                    .receiver(ksClassDeclaration.toClassName())
-                    .returns(List::class.asClassName().parameterizedBy(RegisteredTool::class.asClassName()))
-                    .addParameter("adapter", ServerAdapter::class)
-                    .run {
-                        addStatement("return listOf(")
-                        toolFuncNameList.forEach { funcName ->
-                            addStatement("adapter.%M(\"$funcName\", null, this::$funcName),", makeTool)
-                        }
-                        addStatement(")")
-                    }.build()
-                val fileSpec = FileSpec.builder(
+                val fileSpec = buildFileSpec(
                     ksClassDeclaration.packageName.asString(),
                     "Generated\$${ksClassDeclaration.simpleName.asString()}"
-                )
-                    .addFunction(function)
-                    .build()
+                ) {
+                    addFunction("tools") {
+                        receiver(ksClassDeclaration.toClassName())
+                        returns<List<RegisteredTool>>()
+                        addParameter<ServerAdapter>("adapter") {
+                            defaultValue("%T()", ServerAdapter::class)
+                        }
+                        addCode {
+                            add("return listOf(")
+                            toolFuncNameList.forEach { funcName ->
+                                add("adapter.%M(\"$funcName\", null, this::$funcName),", makeTool)
+                            }
+                            add(")")
+                        }
+                    }
+                }
                 fileSpec.writeTo(environment.codeGenerator, Dependencies(false))
             }
         return emptyList()
